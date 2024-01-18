512808849ccb141ac4c0eee221d84cf6a48bc942
Ikram Messadi <driver2011@protonmail.com>
2024-01-18 23:08:35 +0100

    fix(implementation): added  missing randomization step for algorithm 4

--- implementation/biclustering_.py
+++ implementation/biclustering_.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-from .strings import errors
 
 from .bicluster import Bicluster
 
@@ -12,31 +11,44 @@ class Biclustering:
         alpha: float,
         nb_biclusters: int = 3,
     ) -> tuple:
-        self.nb_biclusters = nb_biclusters
-        self.sigma = sigma
-        self.alpha = alpha
+        self._nb_biclusters = nb_biclusters
+        self._sigma = sigma
+        self._alpha = alpha
         self.biclusters = []
-        self.rows_col_thr = np.random.randint(50)
+        self.rows_col_thr = 100
 
     # validating the attributes
     @property
-    def alpha(self, alpha, sigma):
-        if alpha <= 0 or sigma <= 0:
-            raise ValueError("Alpha/Sigma  value must be strictly positive")
+    def sigma(self):
+        return self._sigma
+
+    @sigma.setter
+    def sigma(self, value):
+        if value <= 0:
+            raise ValueError("Sigma value must be positive")
+        self._sigma = value
 
     @property
+    def alpha(self):
+        return self.alpha
+
+    @alpha.setter
+    def alpha(self, alpha):
+        if alpha <= 0:
+            raise ValueError("Alpha value must be strictly positive")
+        self._alpha = alpha
+
+    @property
+    def nb_biclusters(
+        self,
+    ):
+        return self._nb_biclusters
+
+    @nb_biclusters.setter
     def nb_biclusters(self, nb_biclusters):
-        """Decorator to validate the number of biclusters.
-
-        Args:
-            nb_biclusters (Integer): total number of biclusters.
-
-        Raises:
-            ValueError: Error message in the case of number of biclusters is not positive.
-        """
-        # here we assume that the maximum number of bicluster that could be generated from any matrix is 50
-        if nb_biclusters <= 50:
+        if nb_biclusters > 50:
             raise ValueError("Number of biclusters must not be greater than 50")
+        self._nb_biclusters = nb_biclusters
 
     def msr_score(self, submatrix, rows, cols):
         """
@@ -76,13 +88,13 @@ class Biclustering:
             Tuple of rows & columns : Updated sigma bicluster matrix
         """
         nb_rows, nb_columns = matrix.shape
-        t_rows = []
-        t_columns = []
+        t_rows = rows
+        t_columns = cols
         msr, row_msr, column_msr = self.msr_score(matrix, rows, cols)
-        if msr > self.sigma:
+        print("found msr", msr)
+        if msr > self._sigma:
             column_msr_max = np.argmax(column_msr)
             row_msr_max = np.argmax(row_msr)
-
             if row_msr[row_msr_max] > column_msr[column_msr_max]:
                 t_rows = [i for i in range(nb_rows) if i != row_msr_max]
                 t_columns = list(range(nb_columns))
@@ -105,9 +117,16 @@ class Biclustering:
         Returns:
             Narray : The mean square residue score of the columns.
         """
+
         odd_cols = np.array(
             [i for i in range(matrix.shape[1]) if i not in cols]
         )  # extracting the odd columns (j ∉ J)
+
+        odd_rows = np.array([j for j in range(matrix.shape[0]) if j not in rows])
+
+        if len(odd_cols) == 0 and len(odd_rows) == 0:
+            odd_cols = cols
+            odd_cols = rows
         _, _, column_msr = self.msr_score(matrix, rows, odd_cols)
         return column_msr
 
@@ -157,15 +176,16 @@ class Biclustering:
         converged = False
         while not converged:
             # remove the rows with the highest msr
-            rows_to_remove = np.argwhere(row_msr > self.alpha * msr)[:, 0]
+            rows_to_remove = np.argwhere(row_msr > self._alpha * msr)[:, 0]
             rows = np.array([i for i in range(nb_rows) if i not in rows_to_remove])
             msr, row_msr, column_msr = self.msr_score(matrix, rows, cols)
 
-            if msr > self.sigma:
-                columns_to_remove = np.argwhere(column_msr > self.alpha * msr)[:, 0]
+            if msr > self._sigma:
+                columns_to_remove = np.argwhere(column_msr > self._alpha * msr)[:, 0]
                 cols = np.array(
                     [i for i in range(nb_columns) if i not in columns_to_remove]
                 )
+            # checking if nothing has been removed
             if (
                 previous_rows.shape[0] != rows.shape[0]
                 and previous_cols.shape[0] != cols.shape[0]
@@ -209,6 +229,7 @@ class Biclustering:
             _, _, msr = self.msr_score(matrix, rows, cols)
 
             row_msr, inverse_row_msr = self.msr_row_addition(matrix, rows, cols)
+            print(row_msr, inverse_row_msr)
             rows_to_add = np.argwhere(
                 np.logical_or(row_msr <= msr * msr, inverse_row_msr <= msr)
             )[:, 0]
@@ -219,6 +240,42 @@ class Biclustering:
         return rows, cols
 
     # the final algorithm number 4
+
+    def randomize_values(self, o_matrix, brc: tuple):
+        """Replaces the elements of the bicluster with random values in the original matrix A'.
+        The random values are drawn from a normal distribution with mean : mean of the attribute (column in bicluster D) and std : std of the same attribute (column).
+
+
+        Args:
+            o_matrix (NDArray): Original matrix A' (as denoted in step D in the cc algorithm).
+            brc (tuple): A tuple (NDArray,NDArray) representing the rows & columns of bicluster D.
+        Returns:
+            A reset of the original matrix A'  (please refer to step 4 in Algorithm 4 page = 7 of the CC paper.)
+        """
+
+        # NOTE: For this example we will suppose that the matrix is already normalized
+        d_bicluster = o_matrix[brc[0], :]
+        d_bicluster = d_bicluster[
+            :, brc[1]
+        ].flatten()  # extract the elements of the bicluster matrix into a numpy array
+        # Find the indicies of the matching elements in the original matrix.
+        indecies = [np.argwhere(o_matrix == el)[0] for el in d_bicluster]
+        for index in indecies:
+            # Find the column corresponding to the matching element
+
+            r, c = index
+
+            attribute = o_matrix[:, c]  # extract the column from the orginal matrix
+
+            attribute_mean = np.mean(attribute)
+
+            attribute_std = np.std(attribute)
+
+            randomized_val = np.random.normal(
+                loc=attribute_mean, scale=attribute_std
+            )  # Generate a random value w.r.t the normally distributed attribute
+            o_matrix[r, c] = randomized_val
+        return o_matrix
 
     def run(self, matrix):
         """Method to run the biclustering algorithm in order to generate the n number of biclusters.
@@ -235,11 +292,13 @@ class Biclustering:
         original_rows, original_cols = np.array(range(size_rows)), np.array(
             range(size_cols)
         )
-        if size_rows < self.rows_col_thr or size_cols < self.rows_col_thr:
-            raise ValueError(errors.row_value(self.rows_col_thr, size_cols))
+        # if size_rows < self.rows_col_thr or size_cols < self.rows_col_thr:
+        #     raise ValueError(errors.row_value(self.rows_col_thr, size_cols))
+
         # preform multiple node deletion
 
-        for _ in range(self.nb_biclusters):
+        for i in range(self._nb_biclusters):
+            print(f"iteration number : {i}")
             B_rows, B_cols = self.multiple_node_deletion(
                 matrix, original_rows, original_cols
             )
@@ -247,6 +306,10 @@ class Biclustering:
             D_rows, D_cols = self.node_addition(matrix, C_rows, C_cols)
 
             bsc_msr = self.msr_score(matrix, D_rows, D_cols)
+
             bicluster = Bicluster(rows=D_rows, columns=D_cols, msr_score=bsc_msr)
 
             self.biclusters.append(bicluster)
+
+            # randomize the values in rows and columns D in A
+            matrix = self.randomize_values(matrix, (D_rows, D_cols))
